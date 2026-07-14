2b62dfc5565e6b51b1760b8ef1591155560b0bf0
Vikass <vikasgjv@gmail.com>
2026-07-14 21:48:16 +0530

    Update eslint.config.js

--- next-city-expansion/eslint.config.js
+++ next-city-expansion/eslint.config.js
@@ -4,7 +4,6 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-
 export default defineConfig([
   globalIgnores(['dist']),
   {
